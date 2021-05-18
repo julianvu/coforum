bd1168114f05a2a78787c8836dac904ced7781bf
Julian Vu <julianvu@outlook.com>
2021-05-18 12:08:20 -0700

    Add Navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
+    <div class="container-fluid">
+        <a href="/" class="navbar-brand">Coforum</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Posts</a></li>
+            </ul>
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item"><a href="" class="nav-link">Julian Vu</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Log In</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Register</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Log Out</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
