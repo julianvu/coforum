74cd1487f94e162d6b16ea9fdff59dc5271cb5d0
Julian Vu <julianvu@outlook.com>
2021-05-18 15:36:45 -0700

    Add registration functionality

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,11 +3,39 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        // Validate form data
+        $this->validate($request, [
+            'name' => ['required', 'max:255'],
+            'username' => ['required', 'max:255', 'unique:users,username'],
+            'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        // Store User into database
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // Sign in the user
+        auth()->attempt($request->only('email', 'password'));
+
+        // Redirect user away from Register page
+        return redirect()->route('posts');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,25 +8,47 @@
                     @csrf
                     <h2 class="display-6 text-center">Register</h2>
                     <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" name="name" placeholder="Your name" id="name"
+                               class="form-control bg-gray-200 p-2 rounded-3 @error('name') is-invalid @enderror"
+                               value="{{ old('name') }}">
+                        @error('name')
+                        <small class="text-danger mt-2">{{ $message }}</small>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
                         <label for="username" class="form-label">Username</label>
                         <input type="text" name="username" placeholder="Username" id="username"
-                               class="form-control bg-gray-200 p-2 rounded-3" value="">
+                               class="form-control bg-gray-200 p-2 rounded-3 @error('username') is-invalid @enderror"
+                               value="{{ old('username') }}">
+                        @error('username')
+                        <small class="text-danger mt-2">{{ $message }}</small>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email</label>
                         <input type="email" name="email" placeholder="Your email" id="email"
-                               class="form-control bg-gray-200 p-2 rounded-3" value="">
+                               class="form-control bg-gray-200 p-2 rounded-3 @error('email') is-invalid @enderror"
+                               value="{{ old('email') }}">
+                        @error('email')
+                        <small class="text-danger mt-2">{{ $message }}</small>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password</label>
                         <input type="password" name="password" placeholder="Choose a password" id="password"
-                               class="form-control bg-gray-200 p-2 rounded-3" value="">
+                               class="form-control bg-gray-200 p-2 rounded-3 @error('password') is-invalid @enderror"
+                               value="">
+                        @error('password')
+                        <small class="text-danger mt-2">{{ $message }}</small>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="password_confirmation" class="form-label">Confirm password</label>
                         <input type="password" name="password_confirmation" placeholder="Repeat your password"
                                id="password_confirmation"
-                               class="form-control bg-gray-200 p-2 rounded-3" value="">
+                               class="form-control bg-gray-200 p-2 rounded-3"
+                               value="">
                     </div>
                     <div>
                         <button type="submit" class="btn btn-primary px-3 py-2 rounded-3 font-bold w-100">Register
