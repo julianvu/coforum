7ac9fb29896d1a42eec2200af416a7b40292a521
Julian Vu <julianvu@outlook.com>
2021-05-18 18:44:35 -0700

    Protect logout route

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() {
         auth()->logout();
         return redirect()->route('posts');
